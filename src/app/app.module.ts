db318a797cac2f37b22995db5d9d26b71be3b2cf
Juan31452 <jcs1011@gmail.com>
2021-11-30 19:13:27 -0500

    proyecto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
