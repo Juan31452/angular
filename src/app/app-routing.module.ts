c6dceb1bd48fda37a9013ebda268188ca8750889
Juan31452 <jcs1011@gmail.com>
2021-12-02 19:03:56 -0500

    se implementa router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 
+ const routes: Routes = [
+   {path:'home',component:HomeComponent},
+   {path:'acerca',component:AboutComponent},
+   {path:'contacto',component:ContactoComponent},
+   {path:'**',redirectTo:'home'}
+   
+ ]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+  RouterModule.forRoot(routes)  
+  ],
+  exports:[
+    RouterModule
   ]
 })
-export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './componentes/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     ContactoComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
